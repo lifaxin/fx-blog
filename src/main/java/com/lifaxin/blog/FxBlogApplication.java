8b74977dc66d3376f0936807ce51579207b6a61e
lifaxin <lifaxin@lexue.com>
2020-07-16 21:54:45 +0800

    init

---
+++ src/main/java/com/lifaxin/blog/FxBlogApplication.java
@@ -0,0 +1,13 @@
+package com.lifaxin.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FxBlogApplication.class, args);
+    }
+
+}
